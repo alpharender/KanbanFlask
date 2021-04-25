754aa5dee8bcb449e1a0505959345538573091ea
Zac <z.m.rogers@gmail.com>
2021-04-25 22:21:50 +1200

    Removed login stuff

--- app.py
+++ app.py
@@ -26,34 +26,34 @@ def index():
     return render_template("index.html")
 
 
-@app.before_request
-def before_request():
-    if "user_id" in session:
-        user = [x for x in users if x.id == session['user_id']][0]
-        g.user = user
+# @app.before_request
+# def before_request():
+#     if "user_id" in session:
+#         user = [x for x in users if x.id == session['user_id']][0]
+#         g.user = user
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        session.pop('user_id', None)
-        username = request.form["username"]
-        password = request.form["password"]
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
+#     if request.method == "POST":
+#         session.pop('user_id', None)
+#         username = request.form["username"]
+#         password = request.form["password"]
 
-        user = [x for x in users if x.username == username][0] #assumes unique usernames
+#         user = [x for x in users if x.username == username][0] #assumes unique usernames
 
-        if user and user.password == password:
-            session['user_id'] = user.id
-            return redirect(url_for("profile"))
+#         if user and user.password == password:
+#             session['user_id'] = user.id
+#             return redirect(url_for("profile"))
 
-        return redirect(url_for('login'))
+#         return redirect(url_for('login'))
 
-    return render_template("login.html")
+#     return render_template("login.html")
 
 
-@app.route("/profile")
-def profile():
-    if not g.user:
-        return redirect(url_for('login'))
+# @app.route("/profile")
+# def profile():
+#     if not g.user:
+#         return redirect(url_for('login'))
 
-    return render_template("profile.html")
+#     return render_template("profile.html")
 
